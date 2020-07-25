2de2b7b4d83a4408188a87241590f2c194ef3af0
Rodion <oellekon@mail.ru>
2020-07-25 20:13:08 +0300

    fix input, add get_article function

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+import time
 
-
+SCROLL_PAUSE_TIME = 3
 path = 'geckodriver.exe'
 opt = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(executable_path=path, options=opt)
@@ -12,7 +13,7 @@ driver = webdriver.Firefox(executable_path=path, options=opt)
 
 def login():  # функция для авторизации
     username = input("Введите логин от вк ")
-    password = input("Введите пароль от вк")
+    password = input("Введите пароль от вк ")
     driver.get("https://vk.com/login")
     login_button = driver.find_element_by_id("email")
     login_button.send_keys(username)
@@ -26,3 +27,28 @@ def login():  # функция для авторизации
         print("Ошибка выполнения, проверьте входные данные")
     else:
         print("Авторизация прошла успешно")
+
+
+def get_articles():  # Функция для получения ссылок на все статьи
+    links_dict = []
+    driver.get("https://vk.com/@yvkurse")
+    # Получает высоту документа
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Скроллит до низу
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Ожидает прогрузку страницы
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Сравнивает новую длину и максимальную, в случае чего цикл повторяется
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    result = driver.find_elements_by_css_selector(".author_page_block a")
+    for i in result:
+        links_dict.append(i.get_attribute("href"))
+    return links_dict  # Возвращает список с ссылками на все статьи
