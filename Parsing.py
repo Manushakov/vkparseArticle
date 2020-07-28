75997d59d83f084a8ad7e7ba284833443e7e6b2b
Rodion <oellekon@mail.ru>
2020-07-28 13:48:30 +0300

    Replace imports by pep8

--- Parsing.py
+++ Parsing.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -6,7 +7,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
-import sys
 
 
 PAUSE_TIME = 3
